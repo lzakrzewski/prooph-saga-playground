860ca29954aeffef381456e03d8094084daed2a5
lzakrzewski <lucaszz111@gmail.com>
2017-11-18 23:39:03 +0000

    Project setup

---
+++ tests/acceptance/AcceptanceTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\acceptance;
+
+use PHPUnit\Framework\TestCase;
+
+class AcceptanceTest extends TestCase
+{
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/unit/UnitTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+}
